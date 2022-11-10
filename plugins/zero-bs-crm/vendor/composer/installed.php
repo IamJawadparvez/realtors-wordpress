92515486c8f0dcb18a0ff63faf78b0d0057cc91d
Jawad Parvez <iamjawadparvez@gmail.com>
2022-11-10 14:17:34 -0600

    Updated

---
+++ plugins/zero-bs-crm/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/jetpackcrm',
+        'pretty_version' => 'v5.1.0',
+        'version' => '5.1.0.0',
+        'reference' => '5d9b205b8578ae0a1579c69c5a4b04a5ceb97fbf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpackcrm' => array(
+            'pretty_version' => 'v5.1.0',
+            'version' => '5.1.0.0',
+            'reference' => '5d9b205b8578ae0a1579c69c5a4b04a5ceb97fbf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/woocommerce' => array(
+            'pretty_version' => '3.1.0',
+            'version' => '3.1.0.0',
+            'reference' => 'd3b292f04c0b3b21dced691ebad8be073a83b4ad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../automattic/woocommerce',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
